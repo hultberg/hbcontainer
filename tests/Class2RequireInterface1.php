08d1145ab89a66ebfc5fef1b19efe6099758205a
Edvin Hultberg <hultberg@mittnett.net>
2018-05-07 22:23:19 +0200

    Fix phpstan error and dont report progress in ci

--- tests/Class2RequireInterface1.php
+++ tests/Class2RequireInterface1.php
@@ -4,7 +4,10 @@ namespace HbLib\Container\Tests;
 
 class Class2RequireInterface1
 {
+    private $interface;
+    
     public function __construct(Interface1 $interface)
     {
+        $this->interface = $interface;
     }
 }
