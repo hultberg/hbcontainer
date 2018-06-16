72700b3d9b87460d56b2760f8d069af364a4792c
Edvin Hultberg <hultberg@mittnett.net>
2018-06-16 12:44:50 +0200

    phpstan: Fix unused parameter

--- tests/ClassWithDefaultArgument.php
+++ tests/ClassWithDefaultArgument.php
@@ -4,7 +4,10 @@ namespace HbLib\Container\Tests;
 
 class ClassWithDefaultArgument
 {
+    private $bag;
+
     public function __construct(array $bag = array())
     {
+        $this->bag = $bag;
     }
 }
