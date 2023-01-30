d4a3aa3c3442424dba7ffeff7484df203012877e
Edvin Hultberg <hultberg@mittnett.net>
2023-01-30 21:05:37 +0100

    Update phpstan, phpunit, and config

---
+++ bin/podman-phpunit82.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+set -e
+
+podman run \
+  --interactive \
+  --tty \
+  --rm \
+  --volume ./:/app \
+  --workdir /app \
+  docker.io/library/php:8.2-cli \
+  vendor/bin/phpunit "$@"
+
